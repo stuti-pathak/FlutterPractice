4536fbf661667b00ca5d83d58d0ee4ab74f9efe1
stuti-pathak <stuti.pathak@bridgelabz.com>
2021-09-23 18:23:21 +0530

    Added: dividing larger widget to smaller widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import './question.dart';
-import './answer.dart';
+import './quiz.dart';
+import './result.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,6 +13,20 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final _questions = const [
+    {
+      'questionText': 'What\'s your favourite color ?',
+      'answers': ['Black', 'Orange', 'Red']
+    },
+    {
+      'questionText': 'What\'s your favourite animal ?',
+      'answers': ['Rabbit', 'Dog', 'Cat']
+    },
+    {
+      'questionText': 'who is your favourite actor ?',
+      'answers': ['Jhonny Dep', 'Robert Pattison', 'Christene Bale']
+    },
+  ];
   var _questionIndex = 0;
 
   void _answerQuestion() {
@@ -24,34 +38,17 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    const questions = [
-      {
-        'questionText': 'What\'s your favourite color ?',
-        'answers': ['Black', 'Orange', 'Red']
-      },
-      {
-        'questionText': 'What\'s your favourite animal ?',
-        'answers': ['Rabbit', 'Dog', 'Cat']
-      },
-      {
-        'questionText': 'who is your favourite actor ?',
-        'answers': ['Jhonny Dep', 'Robert Pattison', 'Christene Bale']
-      },
-    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("My First App"),
         ),
-        body: Column(
-          children: [
-            Question(questions[_questionIndex]['questionText'].toString()),
-            ...(questions[_questionIndex]['answers'] as List<String>)
-                .map((answer) {
-              return Answer(_answerQuestion, answer);
-            }).toList()
-          ],
-        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                questions: _questions,
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex)
+            : Result(),
       ),
     );
   }
